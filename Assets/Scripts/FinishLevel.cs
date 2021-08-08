0f0b803c23a2e7a32105cf3c26f0782a86c22d88
TerraDev64 <TerraDev64@gmail.com>
2021-08-08 21:30:52 +0200

    reformated code

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -2,21 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLevel : MonoBehaviour {
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            print("HAHA");
-        }
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+  }
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+  // Start is called before the first frame update
+  void Start() {
+  }
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
+  // Update is called once per frame
+  void Update() {
+  }
 }
