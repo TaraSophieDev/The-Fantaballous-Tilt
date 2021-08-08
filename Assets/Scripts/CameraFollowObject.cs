3fe3db86c43f43b6917960008c066fc82d0fa485
TerraDev64 <TerraDev64@gmail.com>
2021-08-08 21:30:27 +0200

    follows ball better, also added zoom.

--- Assets/Scripts/CameraFollowObject.cs
+++ Assets/Scripts/CameraFollowObject.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CameraFollowObject : MonoBehaviour {
   
   [SerializeField] private GameObject anchor;
-  [SerializeField] private GameObject ball;
-  [SerializeField] private Vector3 offset;
-  
+  [SerializeField] private float zoom = 1f;
+  [Range(0.01f, 1f)] private float followSpeed = 0.1f;
+
+  private GameObject ball;
+  private Vector3 offset;
+
   public bool isBallFollowed = false;
   // Start is called before the first frame update
 
@@ -24,27 +29,42 @@ public class CameraFollowObject : MonoBehaviour {
     if (Gamepad.current != null) {
       if(Gamepad.current.selectButton.wasPressedThisFrame)
         BoolSwitcher();
+
+      if (Gamepad.current.rightStick.up.ReadValue() > 0.0f)
+        zoom -= 0.01f;
+      else if (Gamepad.current.rightStick.down.ReadValue() > 0.0f)
+        zoom += 0.01f;
+      
     } else {
       // Select button
       if (Keyboard.current.altKey.wasPressedThisFrame)
         BoolSwitcher();
+
+      if (Keyboard.current.upArrowKey.isPressed)
+        zoom -= 0.01f;
+      else if (Keyboard.current.downArrowKey.isPressed)
+        zoom += 0.01f;
     }
   }
-  void Start()
-  {
+  void Start() {
+    ball = GameObject.FindWithTag("Player");
+    
     BoolSwitcher();
     offset = transform.position - ball.transform.position;
     offset = transform.position - anchor.transform.position;
   }
 
   // Update is called once per frame
-  void Update() {
+  void FixedUpdate() {
     HandleInput();
+
+    //Limit Zoom
+    zoom = Mathf.Clamp(zoom, 0.1f, 1);
 
     // TODO: Lerp this
     if (!isBallFollowed) 
-      transform.position = anchor.transform.position + offset;
+      transform.position += ((anchor.transform.position + offset * zoom) - transform.position) * followSpeed;
     else
-      transform.position = ball.transform.position + offset;
+      transform.position += ((ball.transform.position + offset * zoom) - transform.position) * followSpeed;
   }
 }
