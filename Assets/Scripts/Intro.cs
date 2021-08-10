912c885c313f78004d70ad8a3e59332af064570c
TerraDev64 <TerraDev64@gmail.com>
2021-08-10 17:03:35 +0200

    added skip intro function

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,17 +1,33 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour {
 
   public int time = 0;
+
+  private void LoadNextScene() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+  
   void Start() {
     StartCoroutine(WaitUntilIntroFinished());
+  }
+
+  private void Update() {
+    if (Gamepad.current != null) {
+      if (Gamepad.current.aButton.isPressed)
+        LoadNextScene();
+    }
+    else if (Keyboard.current.spaceKey.isPressed)
+      LoadNextScene();
   }
 
   IEnumerator WaitUntilIntroFinished() {
     yield return new WaitForSeconds(time);
 
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    LoadNextScene();
   }
 }
