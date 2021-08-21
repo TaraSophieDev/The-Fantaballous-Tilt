766f317460b3d532fc358c005524bee22e448637
TerraDev64 <TerraDev64@gmail.com>
2021-08-22 00:13:25 +0200

    removed unused code

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,30 +6,5 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour {
-  public int lifePoints = 5;
-
-  public TextMeshProUGUI healthNumberText;
-
-  public int getLifePoints() {
-    return lifePoints;
-  }
   
-  public void removeLifePoint() {
-    lifePoints -= 1;
-  }
-  
-  public void Die() {
-    if (lifePoints == 0)
-      SceneManager.LoadScene(1);
-    //switch to death scene/viewport idk
-  }
-
-  void Start() {
-    healthNumberText = FindObjectOfType<TextMeshProUGUI>();
-  }
-
-
-  void Update() {
-    healthNumberText.text = $"x{lifePoints}";
-  }
 }
