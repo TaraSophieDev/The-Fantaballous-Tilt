ea8d1ef08adc5e543c126a436f46aa0faf30f924
TerraDev64 <TerraDev64@gmail.com>
2021-08-22 00:14:47 +0200

    added lifes functionality

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LifeManager : MonoBehaviour {
+
+  public int startLifes;
+  public int lifeCounter;
+
+  private TextMeshProUGUI healthPointText;
+  private void Start() {
+    healthPointText = GetComponent<TextMeshProUGUI>();
+    lifeCounter = startLifes;
+  }
+  private void Update() {
+    healthPointText.text = "x " + lifeCounter;
+    
+    if (lifeCounter == 0)
+      Die();
+  }
+
+  public void TakeLife() {
+    lifeCounter -= 1;
+  }
+
+  private void Die() {
+    SceneManager.LoadScene(1);
+    //switch to death scene/viewport idk
+  }
+}
