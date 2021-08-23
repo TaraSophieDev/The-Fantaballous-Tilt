b12a9a2d68db3aaab53e05fac78ae523a21a009a
TerraDev64 <TerraDev64@gmail.com>
2021-08-24 00:46:34 +0200

    added pause menu functionality

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+  void Start() {
+  }
+
+  void Update() {
+  }
+
+  public void ResumeGame() {
+    
+  }
+  
+  public void OpenOptionsMenu() {
+    
+  }
+  
+  public void ExitToMainMenu() {
+    SceneManager.LoadScene(1);
+  }
+}
