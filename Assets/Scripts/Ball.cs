525032cbc27c3720756f50ea10f4d71e6af402cb
TerraDev64 <TerraDev64@gmail.com>
2021-08-25 11:30:08 +0200

    removed unused imports

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour {
   
