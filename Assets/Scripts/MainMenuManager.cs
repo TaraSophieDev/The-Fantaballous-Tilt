f007d41d79b9d5740d16ac5bc2afe58fa5952367
TerraDev64 <TerraDev64@gmail.com>
2021-08-31 20:41:19 +0200

    added switch layer function to change layers and selected objects thx to jam

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Linq;
 
 public class MainMenuManager : MonoBehaviour {
-  private void CloseGame() {
+
+  [Serializable]
+  public class MenuLayer {
+    public string Name;
+    public GameObject UI;
+    public GameObject firstSelectedGO;
+  }
+  
+  [SerializeField] private MenuLayer[] m_MenuLayers;
+
+  [SerializeField] private EventSystem eventSystem;
+  
+  // [SerializeField] private GameObject MainMenu;
+  // [SerializeField] private GameObject LevelSelectMenu;
+  // [SerializeField] private GameObject CreditsMenu;
+  // [SerializeField] private GameObject OptionsMenu;
+  public void CloseGame() {
     Application.Quit();
+  }
+
+  private void Start() {
+    //MainMenu.transform.GetComponentInChildren<Button>().gameObject
+  }
+
+  // Thanks a lot Jam <3
+  public void SwitchLayer(string layerName) {
+    foreach (var layer in m_MenuLayers) {
+      layer.UI.SetActive(false);
+    }
+    
+    MenuLayer menuLayer = m_MenuLayers.ToList().Find(x => x.Name == layerName);
+    menuLayer.UI.SetActive(true);
+    eventSystem.SetSelectedGameObject(menuLayer.firstSelectedGO);
   }
 }
