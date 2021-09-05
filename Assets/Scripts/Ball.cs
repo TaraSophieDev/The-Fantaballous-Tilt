1bed4f90866029349cb19dca0a6415996e3b60c3
TerraDev64 <TerraDev64@gmail.com>
2021-09-05 22:07:52 +0200

    -

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,7 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Ball : MonoBehaviour {
+  private GameObject pauseMenu;
+
+  private void ActivatePauseMenu() {
+    if (pauseMenu.activeSelf == false)
+      pauseMenu.SetActive(true);
+    else {
+      pauseMenu.SetActive(false);
+    }
+  }
   
+  private void Start() {
+    pauseMenu = GameObject.FindWithTag("Menu");
+    pauseMenu.SetActive(false);
+  }
+  
+  private void Update() {
+    if (Gamepad.current != null && Gamepad.current.startButton.wasPressedThisFrame || Keyboard.current.escapeKey.wasPressedThisFrame)
+      ActivatePauseMenu();
+  }
 }
