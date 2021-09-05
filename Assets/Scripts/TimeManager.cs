301bcc0372a69280cdbcc8d4f342c5f7a6b76b66
TerraDev64 <TerraDev64@gmail.com>
2021-09-05 22:08:26 +0200

    added check state

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -6,23 +6,36 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour {
 
-  private double currentTime;
+  public double currentTime;
   private double startingTime = 0.0;
+
+  private bool timerOn = false;
 
   private TextMeshProUGUI timerText;
 
-  void StartTimer() {
+  private void StartTimer() {
+    // if (timerOn)
+    print("timer still on");
     currentTime = Time.timeAsDouble - startingTime;
   }
+
+  public void StopTimer() {
+    timerOn = false;
+    print("stop time!!!!!!");
+  }
   void Start() {
+    timerOn = true;
     currentTime = startingTime;
     timerText = GetComponent<TextMeshProUGUI>();
   }
   
   void Update() {
-    StartTimer();
+    print(timerOn);
+    if (timerOn)
+      StartTimer();
+    // else if (!timerOn)
+      //print("timer off");
     currentTime = Math.Round(currentTime, 2);
     timerText.text = "Time: " + Convert.ToString(currentTime);
-    print(currentTime);
   }
 }
