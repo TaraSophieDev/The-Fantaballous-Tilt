5657a1e448dd93de356560652e2374e63811d75d
TerraDev64 <TerraDev64@gmail.com>
2021-09-05 22:36:57 +0200

    ß

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -3,16 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class FinishLevel : MonoBehaviour {
+  
+  [SerializeField] private TimeManager timeManager;
+
+  private double time;
+
   private void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("Player")) {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      timeManager.StopTimer();
+      time = timeManager.currentTime;
+      print(time);
+      //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
   }
 
   // Start is called before the first frame update
   void Start() {
+    //timeManager = GameObject.FindWithTag("Timer");
   }
 
   // Update is called once per frame
