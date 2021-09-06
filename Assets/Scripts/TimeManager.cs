9d7015d431220d6b53ee2a78329196ab099af747
TerraDev64 <TerraDev64@gmail.com>
2021-09-06 19:09:06 +0200

    fixed time not resetting bug

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -6,36 +6,55 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour {
 
+  private GameObject pauseMenu;
+
   public double currentTime;
-  private double startingTime = 0.0;
+  private double startingTime;
 
   private bool timerOn = false;
 
   private TextMeshProUGUI timerText;
 
   private void StartTimer() {
-    // if (timerOn)
-    print("timer still on");
-    currentTime = Time.timeAsDouble - startingTime;
+    currentTime = Time.timeSinceLevelLoad - startingTime;
   }
 
   public void StopTimer() {
     timerOn = false;
-    print("stop time!!!!!!");
   }
   void Start() {
+    //startingTime = 0.0;
+    // timerOn = true;
+    // currentTime = startingTime;
+    // timerText = GetComponent<TextMeshProUGUI>();
+    // pauseMenu = GameObject.FindWithTag("Menu");
+  }
+
+  private void ResetValues() {
+    startingTime = 0.0;
+    currentTime = 0.0;
     timerOn = true;
     currentTime = startingTime;
-    timerText = GetComponent<TextMeshProUGUI>();
   }
   
+  private void Awake() {
+    timerText = GetComponent<TextMeshProUGUI>();
+    pauseMenu = GameObject.FindWithTag("Menu");
+    ResetValues();
+  }
+
   void Update() {
+    if (pauseMenu.activeSelf == true)
+      timerOn = false;
+    else
+      timerOn = true;
+      
     print(timerOn);
     if (timerOn)
       StartTimer();
     // else if (!timerOn)
       //print("timer off");
-    currentTime = Math.Round(currentTime, 2);
+      currentTime = Math.Round(currentTime, 2);
     timerText.text = "Time: " + Convert.ToString(currentTime);
   }
 }
