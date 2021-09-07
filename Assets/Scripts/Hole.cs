bce957d6df9ebc6c341375cd712a6e9a79c4aaf4
TerraDev64 <TerraDev64@gmail.com>
2021-09-07 18:38:29 +0200

    commented stuff out

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -7,19 +7,18 @@ public class Hole : MonoBehaviour {
   private GameObject ball;
   private GameObject spawnPoint;
 
-  private LifeManager lifeSystem;
+  //private LifeManager lifeSystem;
   // [SerializeField] private GameObject ball;
   // [SerializeField] private GameObject spawnPoint;
 
   private void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("Player")) {
       ball.transform.position = spawnPoint.transform.position;
-      lifeSystem.TakeLife();
     }
   }
 
   void Start() {
-    lifeSystem = FindObjectOfType<LifeManager>();
+    //lifeSystem = FindObjectOfType<LifeManager>();
     
     ball = GameObject.FindWithTag("Player");
     spawnPoint = GameObject.FindWithTag("SpawnPoint");
