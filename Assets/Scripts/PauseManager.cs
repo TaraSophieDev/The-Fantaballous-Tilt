bab2cb1f6c075aa142852415c0bbb9f50db38c59
TerraDev64 <TerraDev64@gmail.com>
2021-09-07 18:39:36 +0200

    meta

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,27 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour {
+  
   void Start() {
   }
+  
 
   void Update() {
+    if (this.gameObject.activeSelf == true)
+      Time.timeScale = 0;
   }
 
   public void ResumeGame() {
+    Time.timeScale = 1;
+    this.gameObject.SetActive(false);
     
   }
 
   public void RestartLevel() {
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
-  
-  public void OpenOptionsMenu() {
-    
-  }
-  
+
   public void ExitToMainMenu() {
     SceneManager.LoadScene(1);
   }
