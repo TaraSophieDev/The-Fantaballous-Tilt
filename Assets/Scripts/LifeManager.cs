b67ec8be67d3b1dbb5f0844e254b836b53388272
TerraDev64 <TerraDev64@gmail.com>
2021-09-07 18:38:12 +0200

    commented everything out bc not needed

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -6,27 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class LifeManager : MonoBehaviour {
 
-  public int startLifes;
-  public int lifeCounter;
-
-  private TextMeshProUGUI healthPointText;
-  private void Start() {
-    healthPointText = GetComponent<TextMeshProUGUI>();
-    lifeCounter = startLifes;
-  }
-  private void Update() {
-    healthPointText.text = "x " + lifeCounter;
-    
-    if (lifeCounter == 0)
-      Die();
-  }
-
-  public void TakeLife() {
-    lifeCounter -= 1;
-  }
-
-  private void Die() {
-    SceneManager.LoadScene(1);
-    //switch to death scene/viewport idk
-  }
+  // public int startLifes;
+  // public int lifeCounter;
+  //
+  // private TextMeshProUGUI healthPointText;
+  // private void Start() {
+  //   healthPointText = GetComponent<TextMeshProUGUI>();
+  //   lifeCounter = startLifes;
+  // }
+  // private void Update() {
+  //   healthPointText.text = "x " + lifeCounter;
+  //   
+  //   if (lifeCounter == 0)
+  //     Die();
+  // }
+  //
+  // public void TakeLife() {
+  //   lifeCounter -= 1;
+  // }
+  //
+  // private void Die() {
+  //   SceneManager.LoadScene(1);
+  //   //switch to death scene/viewport idk
+  // }
 }
