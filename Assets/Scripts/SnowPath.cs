e5748a81c896158b32490a1ffd414378c7e4b8b8
TerraDev64 <TerraDev64@gmail.com>
2021-09-08 19:24:54 +0200

    snow path functions

--- Assets/Scripts/SnowPath.cs
+++ Assets/Scripts/SnowPath.cs
@@ -4,32 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SnowPath : MonoBehaviour {
+  private GameObject ball;
+  private Rigidbody ballRigidbody;
 
-    private GameObject ball;
-    private Rigidbody ballRigidbody;
+  private void Awake() {
+    ball = GameObject.FindWithTag("Player");
+    ballRigidbody = ball.GetComponent<Rigidbody>();
+  }
 
-    private void Awake() {
-        ball = GameObject.FindWithTag("Player");
-        ballRigidbody = ball.GetComponentInChildren<Rigidbody>();
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+      ballRigidbody.drag = 5;
     }
+  }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            ballRigidbody.drag = 5;
-        }
+  private void OnTriggerExit(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+      ballRigidbody.drag = 0;
     }
-
-    private void OnTriggerExit(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            ballRigidbody.drag = 0;
-        }
-    }
-
-    void Start() {
-        
-    }
-
-    void Update() {
-        
-    }
+  }
 }
