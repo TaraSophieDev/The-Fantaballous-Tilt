1a2283b1a33ea09d3cac578059b2546512e4d090
TerraDev64 <TerraDev64@gmail.com>
2021-09-14 22:42:21 +0200

    added fix for event system

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -2,19 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class FinishLevel : MonoBehaviour {
   
   [SerializeField] private TimeManager timeManager;
   [SerializeField] private GameObject NextLevelPrompt;
+  private EventSystem eventSystem;
+  [SerializeField] private GameObject button;
+  
 
   private double time;
+
+  private void Awake() {
+    eventSystem = EventSystem.current;
+  }
 
   private void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("Player")) {
       NextLevelPrompt.SetActive(true);
+      eventSystem.SetSelectedGameObject(button);
       Time.timeScale = 0f;
       //timeManager.StopTimer();
       //time = timeManager.currentTime;
