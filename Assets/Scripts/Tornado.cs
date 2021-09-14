f0d6d44a50c116d468438c6f300dd67c585affd3
TerraDev64 <TerraDev64@gmail.com>
2021-09-14 22:44:27 +0200

    added gravity physics (kinda like planets and orbits)

--- Assets/Scripts/Tornado.cs
+++ Assets/Scripts/Tornado.cs
@@ -1,40 +1,53 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using HauntedPSX.RenderPipelines.PSX.Runtime;
-using JetBrains.Rider.Unity.Editor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Tornado : MonoBehaviour {
   private GameObject ball;
   private Rigidbody ballRigidbody;
+  private Rigidbody tornadoRigidbody;
   private GameObject spawnPoint;
+
+  private float objectDistance;
+  private Vector3 direction;
   
-  [SerializeField] private float pullStrength = 0.5f;
+  //[SerializeField] private float pullStrength = 0.5f;
   [SerializeField] private int pullDistance = 10;
 
   private void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("Player")) {
-      ball.transform.position = spawnPoint.transform.position;
+      //ball.transform.position = spawnPoint.transform.position;
     }
   }
   
   private void Awake() {
     ball = GameObject.FindWithTag("Player");
     spawnPoint = GameObject.FindWithTag("SpawnPoint");
-    
+
+    tornadoRigidbody = this.GetComponent<Rigidbody>();
     ballRigidbody = ball.GetComponent<Rigidbody>();
   }
 
-  private void PullBallToTornado() {
-    print("turned on function");
-    ballRigidbody.AddForce((transform.position - ball.transform.position) * pullStrength * Time.fixedTime, ForceMode.Force);
+  private void PullBallToTornado(float distance) {
+    //ballRigidbody.AddForce((transform.position - ball.transform.position) * pullStrength * Time.fixedDeltaTime, ForceMode.Force);
+    float forceMagnitude = (ballRigidbody.mass * tornadoRigidbody.mass) / (distance * distance);
+    Vector3 force = direction.normalized * forceMagnitude;
+    
+    ballRigidbody.AddForce(force);
   }
-  
+
+  // private void PushBallAway(float distance) {
+  //   ballRigidbody.AddForce((transform.position + ball.transform.position) * pullStrength * 10 * Time.fixedDeltaTime, ForceMode.Impulse);
+  // }
+
+
+  private void FixedUpdate() {
+    direction = transform.position - ball.transform.position;
+    objectDistance = Vector3.Distance(transform.position, ball.transform.position);
+  }
 
   void Update() {
-    if (Vector3.Distance(transform.position, ball.transform.position) < pullDistance)
-      PullBallToTornado();
+    if (objectDistance < pullDistance)
+      PullBallToTornado(objectDistance);
   }
 }
