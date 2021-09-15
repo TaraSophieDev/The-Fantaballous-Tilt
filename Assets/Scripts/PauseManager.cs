9abf3b302d034c04b7bc8b4c8bacef18ae740995
TerraDev64 <TerraDev64@gmail.com>
2021-09-15 19:02:42 +0200

    removed start function

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour {
-  
-  void Start() {
-  }
-  
 
   void Update() {
     if (this.gameObject.activeSelf == true)
