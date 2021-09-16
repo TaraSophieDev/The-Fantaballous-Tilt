67b96d3ad1a7998ced9fc266d5ca3adcbe04e372
TerraDev64 <TerraDev64@gmail.com>
2021-09-16 19:02:58 +0200

    fixed platform not rotating in menu

--- Assets/Scripts/MenuRotation.cs
+++ Assets/Scripts/MenuRotation.cs
@@ -11,6 +11,7 @@ public class MenuRotation : MonoBehaviour {
   // Start is called before the first frame update
   void Start() {
     rb = GetComponent<Rigidbody>();
+    Time.timeScale = 1;
   }
 
   // Update is called once per frame
