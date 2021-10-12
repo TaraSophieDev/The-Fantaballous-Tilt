a6b1d126f935cd99b7f519b53e4e33ad2f3004c7
TerraDev64 <TerraDev64@gmail.com>
2021-10-12 22:13:44 +0200

    added new models

--- Assets/Scripts/Igloo.cs
+++ Assets/Scripts/Igloo.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Igloo : MonoBehaviour {
-  void Start() {
+  private GameObject ball;
+  private GameObject igloo;
+
+  //private LifeManager lifeSystem;
+  // [SerializeField] private GameObject ball;
+  // [SerializeField] private GameObject spawnPoint;
+
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+      ball.transform.position = igloo.transform.position;
+    }
   }
 
-  void Update() {
+  private void Awake() {
+    ball = GameObject.FindWithTag("Player");
+    igloo = GameObject.FindWithTag("SpawnPoint");
   }
 }
