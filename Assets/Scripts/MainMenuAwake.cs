75eff328fba7e312bad51bbe3cd936366975ae1c
TerraDev64 <TerraDev64@gmail.com>
2021-10-25 00:01:56 +0200

    fixed class name

--- Assets/Scripts/MainMenuAwake.cs
+++ Assets/Scripts/MainMenuAwake.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuAwakwe : MonoBehaviour {
+public class MainMenuAwake : MonoBehaviour {
     private void Awake() {
         Time.timeScale = 1;
     }
