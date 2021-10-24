7d54c6dc8b1ab1f5262f1aca649422a575e148d2
TerraDev64 <TerraDev64@gmail.com>
2021-10-24 23:57:56 +0200

    added main menu awake script

---
+++ Assets/Scripts/MainMenuAwake.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAwakwe : MonoBehaviour {
+    private void Awake() {
+        Time.timeScale = 1;
+    }
+}
