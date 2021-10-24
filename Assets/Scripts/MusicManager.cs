dabfcc41550e6fa4607e234716d65ae024897bc3
TerraDev64 <TerraDev64@gmail.com>
2021-10-24 13:32:16 +0200

    fixed bug

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -18,30 +18,31 @@ public class MusicManager : MonoBehaviour {
     audioSource.clip = audioClipArray[audioArray];
     audioSource.Play();
   }
-  
+
+  private void Update() {
+    print(audioSource.clip);
+  }
+
   private void Awake()
   {
     GameObject[] musicObj = GameObject.FindGameObjectsWithTag("MusicPlayer");
 
     switch (SceneManager.GetActiveScene().name) {
       case "Level_1": case "Level_2": case "Level_3": case "Level_4": case "Level_5": case "Level_6":
-        //play forest music
+        playSong(1);
         break;
       case "Level_7": case "Level_8": case "Level_9": case "Level_10": case "Level_11": case "Level_12":
-        //play snow/ice music
-        break;
-      case "Level_13": case "Level_14": case "Level_15": case "Level_16": case "Level_17": case "Level_18":
-        //play desert music
+        playSong(2);
         break;
       default:
         playSong(0);
         break;
     }
 
-    if (SceneManager.GetActiveScene().name == "MainMenu")
-      if (musicObj.Length > 1) {
-        Destroy(this.gameObject);
-      }
-    DontDestroyOnLoad(this.gameObject);
+    // if (SceneManager.GetActiveScene().name == "MainMenu")
+    //   if (musicObj.Length > 1) {
+    //     Destroy(this.gameObject);
+    //   }
+    // DontDestroyOnLoad(this.gameObject);
   }
 }
